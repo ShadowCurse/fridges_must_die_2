354c05a420386bda849a4c116c10b299ca02707c
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2023-12-27 15:07:49 +0000

    moved input handling into game.gd + smooth deceleraton for player

--- game.gd
+++ game.gd
@@ -5,6 +5,8 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
     var level = LEVEL_SCENE.instantiate() as Node3D
     self.add_child(level)
 
@@ -14,3 +16,10 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
     pass
+
+func _input(event: InputEvent) -> void:
+    if event.is_action_pressed("game_esc"):
+        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+    if event.is_action_pressed("game_lmb"):
+        if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
+            Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- player.gd
+++ player.gd
@@ -2,7 +2,8 @@ extends CharacterBody3D
 
 @onready var CAMERA: Camera3D = $camera
 
-const SPEED: float = 5.0
+const SPEED: float = 10.0
+const DECELERATION: float = 5.0
 const JUMP_VELOCITY: float = 4.5
 const MOUSE_SENSE: float = 0.002
 
@@ -10,37 +11,32 @@ const MOUSE_SENSE: float = 0.002
 var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
 
 func _ready():
-    Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+    pass
 
 func _physics_process(delta: float) -> void:
     # Add the gravity.
-    if not is_on_floor():
-        velocity.y -= gravity * delta
+    if not self.is_on_floor():
+        self.velocity.y -= gravity * delta
 
     # Handle jump.
-    if Input.is_action_just_pressed("game_jump") and is_on_floor():
-        velocity.y = JUMP_VELOCITY
+    if Input.is_action_just_pressed("game_jump") and self.is_on_floor():
+        self.velocity.y = JUMP_VELOCITY
 
     # Get the input direction and handle the movement/deceleration.
     # As good practice, you should replace UI actions with custom gameplay actions.
     var input_dir := Input.get_vector("game_left", "game_right", "game_front", "game_back")
-    var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+    var direction := (self.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
     if direction:
-        velocity.x = direction.x * SPEED
-        velocity.z = direction.z * SPEED
+        self.velocity.x = direction.x * SPEED
+        self.velocity.z = direction.z * SPEED
     else:
-        velocity.x = move_toward(velocity.x, 0, SPEED)
-        velocity.z = move_toward(velocity.z, 0, SPEED)
+        self.velocity.x = lerpf(self.velocity.x, 0, DECELERATION * delta)
+        self.velocity.z = lerpf(self.velocity.z, 0, DECELERATION * delta)
 
-    move_and_slide()
+    self.move_and_slide()
     
 func _input(event: InputEvent) -> void:
     if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
         rotate_y(-event.relative.x * MOUSE_SENSE)
         CAMERA.rotate_x(-event.relative.y * MOUSE_SENSE)
         CAMERA.rotation.x = clampf(CAMERA.rotation.x, -deg_to_rad(70), deg_to_rad(70))
-    if event.is_action_pressed("game_esc"):
-        Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-    if event.is_action_pressed("game_lmb"):
-        if Input.mouse_mode == Input.MOUSE_MODE_VISIBLE:
-            Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
