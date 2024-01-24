8221633e53241f82c022fe4c75a65c7418ddd668
ShadowCurse <48793978+ShadowCurse@users.noreply.github.com>
2024-01-24 23:45:35 +0000

    added light to the pickup

--- pickup.gd
+++ pickup.gd
@@ -3,15 +3,22 @@ extends Node3D
 class_name Pickup
 
 @onready var anchor: Node3D = $anchor
+@onready var omni_light_3d: OmniLight3D = $anchor/OmniLight3D
 
 @export var object: PackedScene
+@export var hilight_color: Color
 @export var frequency: float = 0.001
 @export var ampitude: float = 0.3
 @export var rotation_speed: float = 0.1
 
+var attached_object: Node3D
+
 func _ready() -> void:
     var o = object.instantiate() as Node3D
+    self.attached_object = o;
     self.anchor.add_child(o)
+    
+    self.omni_light_3d.light_color = hilight_color
 
 func _process(delta: float) -> void:
     self.anchor.position.y = sin(Time.get_ticks_msec() * frequency) * ampitude
@@ -20,10 +27,9 @@ func _process(delta: float) -> void:
 
 func on_area_3d_body_entered(body: Node3D) -> void:
     if body is Player:
-        var objects = self.anchor.get_children()
-        if objects.size() == 1:
-            self.anchor.remove_child(objects[0])
-            if body.use_pickup(objects[0]):
+        if self.attached_object:
+            self.anchor.remove_child(self.attached_object)
+            if body.use_pickup(self.attached_object):
                 self.queue_free()
             else:
-              self.anchor.add_child(objects[0])
+              self.anchor.add_child(self.attached_object)
